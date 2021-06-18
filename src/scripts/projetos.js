9685fe985c0799badc5f0146ef02e946e6891a89
Davi <davifeitosa.dev@protonmail.com>
2021-06-18 02:00:27 -0300

    feat: add link to social dogs

--- src/scripts/projetos.js
+++ src/scripts/projetos.js
@@ -3,7 +3,7 @@ const projects = [{
     link: 'survey-api',
     status: 'Trabalho em progresso',
     thumb: 'surveyapi.png',
-    description: 'Autem ipsum nam porro corporis rerum. Quis eos dolorem eos itaque inventore commodi labore quia quia. Exercitationem repudiandae officiis neque suscipit non officia eaque itaque enim. Voluptatem officia accusantium nesciunt est omnis tempora consectetur dignissimos. Sequi nulla at esse enim cum deserunt eius.',
+    description: 'A Suvey API é uma API desenvolvida para a criação de sistemas de enquete. Nela você pode criar a sua conta, fazer login e criar e responder enquetes. Este software foi desenvolvido com a intenção de exercitar conhecimentos de arquitetura de software, como: TDD, POO, Arquitetura Limpa e Design Patterns. Este sistema foi desenvolvido com base no treinamento do Rodrigo Manguinho de Arquitetura de Software',
     type: 'backend',
     tools: ['Javascript', 'Node'],
     github: 'https://github.com/feitosadavi/survey-pet-api'
@@ -12,7 +12,7 @@ const projects = [{
     link: 'mybooks',
     status: 'Trabalho em progresso',
     thumb: 'mybooks.png',
-    description: 'Autem ipsum nam porro corporis rerum. Quis eos dolorem eos itaque inventore commodi labore quia quia. Exercitationem repudiandae officiis neque suscipit non officia eaque itaque enim. Voluptatem officia accusantium nesciunt est omnis tempora consectetur dignissimos. Sequi nulla at esse enim cum deserunt eius.',
+    description: 'O MyBooks consiste em uma biblioteca para a locação e agendamento para a busca de livros de uma escola. Neste sistema é possível para o aluno, basicamente, criar a sua conta, fazer login, locar os livros cadastrados e marcar o agendamento para o busca dos livros. Foi desenvolvimento como parte do TCC do meu curso Técnico em Informática.',
     type: 'backend and frontend',
     tools: 'Java, JSP, Bootstrap',
     github: 'https://github.com/feitosadavi/MyBooks'
@@ -20,9 +20,10 @@ const projects = [{
   }, {
     name: 'Social Dogs',
     link: 'social-dogs',
+    url: 'https://social-dogs.herokuapp.com/',
     status: 'Concluído :)',
     thumb: 'socialdogs.png',
-    description: 'Autem ipsum nam porro corporis rerum. Quis eos dolorem eos itaque inventore commodi labore quia quia. Exercitationem repudiandae officiis neque suscipit non officia eaque itaque enim. Voluptatem officia accusantium nesciunt est omnis tempora consectetur dignissimos. Sequi nulla at esse enim cum deserunt eius.',
+    description: 'A Social Dogs é uma rede social feita para cachorros. Nela o dono do pet pode criar uma conta para o seu bichinho, postar fotos, fazer comentários em outros posts e ter estatísticas acerca do seu engajamento. Este sistema foi feito com base no curso de React Completo da Origamid.',
     type: 'frontend',
     tools: ['Javascript', 'React'],
     github: 'https://github.com/feitosadavi/dogs'
